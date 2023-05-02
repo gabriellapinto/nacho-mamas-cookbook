24aadb7c827a905c1de7285817b774ed68d5ff85
Gabby <gabbympinto@gmail.com>
2023-05-01 21:02:28 -0400

    added models and db

---
+++ models/Recipe.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require('sequelize');
+const User = require('./User');
+const sequelize = require('../config/connection');
+
+class Recipe extends Model { };
+
+Recipe.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        instructions: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: User,
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'recipe'
+    }
+);
+
+module.exports = Recipe;
